88f7f9ee6db9c5f7e314b22972c2a11c61e07b99
LaughingLeader <laughingleader@gmail.com>
2021-07-29 10:43:23 -0500

    Figured out a way to teleport newcomes to the void server spawn

--- kubejs/server_scripts/stages/initial.js
+++ kubejs/server_scripts/stages/initial.js
@@ -7,6 +7,15 @@
  * @property {ServerPlayerJS} player
  */
 
+let serverSpawn = {
+	pos: [0.5, 104.0, -127.0],
+	facing: [0.0, 8.6]
+}
+
+let teleportCommand = function(player) {
+	return `/execute in laughpack:void run teleport ${player} ${serverSpawn.pos.join(" ")} ${serverSpawn.facing.join(" ")}`
+}
+
 /**
  * @param {LoggedInEventJS} e
 **/
@@ -27,7 +36,27 @@ let onLoggedIn = function(e)
 
 	if (!e.hasGameStage("void_guest") && !e.player.OP && !e.player.creativeMode) {
 		e.addGameStage("void_guest")
+		e.server.runCommandSilent(teleportCommand(e.player.getName()))
+		// let spawnDimension = e.player.minecraftEntity.func_241141_L_()
+		// if (spawnDimension != null && spawnDimension.path == "overworld") {
+			
+		// }
 	}
+}
+
+/** 
+ * @param {ServerPlayerEntityJS} player
+ * @param {CommandEventJS} e 
+*/
+global.customCommands.spawntest = function (player, cmdParts, e) {
+	let username = e.parseResults.context.source.func_197011_j()[0]
+	//console.info(`${user} (${typeof(user)})`)
+	//global.inspect(user)
+	//global.inspect(e.parseResults.context.source)
+	//global.inspect(e.parseResults.context.source, true)
+	//console.info(`Player name: ${player.entity.getName()}`)
+	e.server.runCommandSilent(teleportCommand(username))
+	e.cancel()
 }
 
 events.listen("player.logged_in", onLoggedIn)
