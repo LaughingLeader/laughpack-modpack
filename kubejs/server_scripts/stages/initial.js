2cf9ead2ebf174475b7c5d0623a52bb4acb57af9
LaughingLeader <laughingleader@gmail.com>
2021-07-29 06:52:09 -0500

    Setup void dimension restrictions using game stages

---
+++ kubejs/server_scripts/stages/initial.js
@@ -0,0 +1,32 @@
+//priority: 99
+
+/**
+ * @typedef LoggedInEventJS
+ * @type {Object}
+ * @property {ServerPlayer} player
+ */
+
+/**
+ * @param {LoggedInEventJS} e
+**/
+let onLoggedIn = function(e)
+{
+	let startingItems = [
+		"minecraft:stone_sword",
+		"minecraft:stone_pickaxe",
+		{ item: "minecraft:apple", count: 6 },
+	]
+
+	if (!e.hasGameStage("starting_items")) {
+		e.addGameStage("starting_items")
+		startingItems.forEach(item=> {
+			e.player.give(item)
+		})
+	}
+
+	if (!e.hasGameStage("void_guest") && !e.player.OP && !e.player.creativeMode) {
+		e.addGameStage("void_guest")
+	}
+}
+
+events.listen("player.logged_in", onLoggedIn)
